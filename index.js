f26b92a288a28b9d05262e67c31aef95e7205067
voncantbetrace-svg <voncantbetrace@gmail.com>
2026-03-24 23:59:04 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+|require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once('ready', () => {
+  console.log(`✅ Logged in as ${client.user.tag}`);
+});
+
+// Simple command
+client.on('messageCreate', message => {
+  if (message.author.bot) return;
+
+  if (message.content === 'earthrock') {
+    message.reply('boom');
+  }
+});
+
+client.login(process.env.TOKEN);
